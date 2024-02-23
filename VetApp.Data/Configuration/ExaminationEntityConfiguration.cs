c7d2277cb51c1734a0cbc00602d9c416f40cbe50
Teodor-Totev <tedkata96@gmail.com>
2024-02-23 18:59:30 +0200

    Edit Seeding Examinations

--- VetApp.Data/Configuration/ExaminationEntityConfiguration.cs
+++ VetApp.Data/Configuration/ExaminationEntityConfiguration.cs
@@ -1,12 +1,19 @@
 ﻿namespace VetApp.Data.Configuration
 {
-    using Microsoft.EntityFrameworkCore;
-    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+	using Microsoft.EntityFrameworkCore;
+	using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-    using VetApp.Data.Models;
+	using VetApp.Data.Models;
 
-    public class ExaminationEntityConfiguration : IEntityTypeConfiguration<Examination>
+	public class ExaminationEntityConfiguration : IEntityTypeConfiguration<Examination>
 	{
+		private readonly VetAppDbContext context;
+
+		public ExaminationEntityConfiguration(VetAppDbContext context)
+		{
+			this.context = context;
+		}
+
 		public void Configure(EntityTypeBuilder<Examination> builder)
 		{
 			builder
@@ -17,30 +24,42 @@
 		{
 			ICollection<Examination> examinations = new HashSet<Examination>();
 
-			Examination examination;
-			examination = new()
+			if (!context.Examinations.Any())
 			{
-				Id = 1,
-                DoctorId = Guid.Parse("67D4E605-D264-48D5-44C9-08DC28F5B9F5"),
-				PatientId = 1,
-			};
-			examinations.Add(examination);
+				Examination examination;
+				examination = new()
+				{
+					Id = 1,
+					Reason = "Primary",
+					CreatedOn = DateTime.UtcNow,
+					Weight = 12,
+					DoctorId = Guid.Parse("67D4E605-D264-48D5-44C9-08DC28F5B9F5"),
+					PatientId = 1,
+				};
+				examinations.Add(examination);
 
-			examination = new()
-			{
-				Id = 2,
-				DoctorId = Guid.Parse("67D4E605-D264-48D5-44C9-08DC28F5B9F5"),
-				PatientId = 2,
-			};
-			examinations.Add(examination);
+				examination = new()
+				{
+					Id = 2,
+					Reason = "Secondary",
+					CreatedOn = DateTime.UtcNow,
+					Weight = 10,
+					DoctorId = Guid.Parse("67D4E605-D264-48D5-44C9-08DC28F5B9F5"),
+					PatientId = 2,
+				};
+				examinations.Add(examination);
 
-			examination = new()
-			{
-				Id = 3,
-				DoctorId = Guid.Parse("67D4E605-D264-48D5-44C9-08DC28F5B9F5"),
-				PatientId = 3,
-			};
-			examinations.Add(examination);
+				examination = new()
+				{
+					Id = 3,
+					Reason = "Primary",
+					CreatedOn = DateTime.UtcNow,
+					Weight = 30,
+					DoctorId = Guid.Parse("67D4E605-D264-48D5-44C9-08DC28F5B9F5"),
+					PatientId = 3,
+				};
+				examinations.Add(examination);
+			}
 
 			return examinations.ToArray();
 		}
