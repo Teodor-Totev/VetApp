39a089a0ed5efa57f0800939ca619e214cfd8c79
Teodor-Totev <tedkata96@gmail.com>
2024-03-04 12:48:06 +0200

    Fix base controller, Add claimpricipal extension for userId

--- VetApp/Controllers/BaseController.cs
+++ VetApp/Controllers/BaseController.cs
@@ -1,13 +1,10 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System.Security.Claims;
 
 namespace VetApp.Controllers
 {
 	[Authorize]
 	public class BaseController : Controller
 	{
-        internal string GetUserId()
-            => User.FindFirstValue(ClaimTypes.NameIdentifier);
     }
 }
---
+++ VetApp/Extensions/ClaimsPrincipalExtansions.cs
@@ -0,0 +1,10 @@
+﻿namespace VetApp.Extensions
+{
+	using System.Security.Claims;
+
+	public static class ClaimsPrincipalExtansions
+	{
+		public static string Id(this ClaimsPrincipal user)
+			=> user.FindFirstValue(ClaimTypes.NameIdentifier);
+	}
+}
