cb6ffed73aa615ecbc61de259808b11fc29c2adc
Teodor-Totev <tedkata96@gmail.com>
2024-03-17 18:20:27 +0200

    Start adding owner all logic with pagination

--- VetApp.Web.ViewModels/Owner/AllOwnersQueryModel.cs
+++ VetApp.Web.ViewModels/Owner/AllOwnersQueryModel.cs
@@ -1,7 +1,28 @@
 ﻿namespace VetApp.Web.ViewModels.Owner
 {
+	using System.ComponentModel.DataAnnotations;
+	using VetApp.Web.ViewModels.Owner.Enums;
+	using VetApp.Web.ViewModels.Patient;
+
 	public class AllOwnersQueryModel
 	{
+        public AllOwnersQueryModel()
+        {
+            this.Owners = new HashSet<OwnerViewModel>();
+        }
 
-	}
+        public string? SearchString { get; set; }
+
+        public int OwnersPerPage { get; set; }
+
+        public int CurrentPage { get; set; }
+
+        public int TotalOwners { get; set; }
+
+		[Display(Name = "Sort owners by:")]
+		[EnumDataType(typeof(OwnerSorting))]
+		public OwnerSorting OwnerSorting { get; set; }
+
+        public IEnumerable<OwnerViewModel> Owners { get; set; }
+    }
 }
---
+++ VetApp.Web.ViewModels/Owner/Enums/OwnerSorting.cs
@@ -0,0 +1,10 @@
+﻿namespace VetApp.Web.ViewModels.Owner.Enums
+{
+	public enum OwnerSorting
+	{
+		OwnerName = 0,
+		OwnerNameDescending = 1,
+		AnimalsCount = 2,
+		AnimalsCountDescending = 3
+	}
+}
