010d5550f2e49a26c0ed5f2f4211714760b222cb
Teodor-Totev <tedkata96@gmail.com>
2024-03-25 20:47:03 +0200

    Added Edit role

---
+++ VetApp.Web.ViewModels/Administration/EditRoleViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace VetApp.Web.ViewModels.Administration
+{
+	using System.Collections.Generic;
+	using System.ComponentModel.DataAnnotations;
+
+	public class EditRoleViewModel
+	{
+		public EditRoleViewModel()
+		{
+			this.Users = new List<string>();
+		}
+
+		public string Id { get; set; } = null!;
+
+		[Required]
+		[Display(Name = "Role name")]
+		public string RoleName { get; set; } = null!;
+
+		public List<string> Users { get; set; }
+	}
+}
